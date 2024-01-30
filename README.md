02b4b617116e08719e537ea8d41f809787ee50fd
whi-ming <whimingj@gmail.com>
2024-01-29 20:36:31 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# myWebsite
+ my official website
---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+//smooth scroll
+const lenis = new Lenis()
+lenis.on('scroll', (e) => {
+  console.log(e)
+})
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
